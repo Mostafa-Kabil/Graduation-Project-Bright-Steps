1c8d3cc827c826e4b96cbb2ff933af721e2ad81f
Moaz Ahmed <mozaahmed0101@gmail.com>
2026-02-26 07:25:01 +0200

    establish database connection

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php 
+try {
+    $connect = new PDO("mysql:host=localhost;dbname=grad;charset=utf8", "root", "");
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit();
+}
+?>
